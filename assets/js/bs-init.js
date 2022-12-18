74dcdae91e73cd46397278e6cd86285f66cd12d5
lingzidi <s181037@chusheklun.edu.hk>
2022-12-18 17:55:40 +0800

    sda
    sd

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -10,10 +10,40 @@ if (window.innerWidth < 768) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+	if ('AOS' in window) {
+		AOS.init();
+	}
 
 	var hoverAnimationTriggerList = [].slice.call(document.querySelectorAll('[data-bss-hover-animate]'));
 	var hoverAnimationList = hoverAnimationTriggerList.forEach(function (hoverAnimationEl) {
 		hoverAnimationEl.addEventListener('mouseenter', function(e){ e.target.classList.add('animated', e.target.dataset.bssHoverAnimate) });
 		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
 	});
+
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bss-tooltip]'));
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+	  return new bootstrap.Tooltip(tooltipTriggerEl);
+	})
+
+	var toastTriggers = document.querySelectorAll('[data-bs-toggle="toast"]');
+
+	for (var toastTrigger of toastTriggers) {
+		toastTrigger.addEventListener('click', function () {
+			var toastSelector = toastTrigger.getAttribute('data-bs-target');
+
+			if (!toastSelector) return;
+
+			try {
+				var toastEl = document.querySelector(toastSelector);
+
+				if (!toastEl) return;
+
+				var toast = new bootstrap.Toast(toastEl);
+				toast.show();
+			}
+			catch(e) {
+				console.error(e);
+			}
+		})
+	}
 }, false);
